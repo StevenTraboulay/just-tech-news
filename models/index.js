e5ea91f16122a37c7e4ec58e3d5af5b9cc97d186
StevenTraboulay <khaleeltraboulay@gmail.com>
2020-11-25 17:58:29 -0500

    module one total

---
+++ models/index.js
@@ -0,0 +1,6 @@
+//importing the user model
+const User = require('./User');
+
+
+//exporting the object with its property 
+module.exports = { User };
---
+++ routes/api/user-routes.js
@@ -0,0 +1,105 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+//all users from the user table
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method)
+    //all users in table = SELECT * FROM users;
+    User.findAll({
+        attributes: { exclude: ['password'] }
+      })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+  });
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+    //finding one specific form selection = SELECR * FROM users WHERE id = 1
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+  });
+
+// POST /api/users
+//this is similiar to
+// INSERT INTO users
+//   (username, email, password)
+// VALUES
+//   ("Lernantino", "lernantino@gmail.com", "password1234");
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({  //create is to craete a new object
+        //req.body and req. parms updated existing data
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    User.update(req.body, { // combines paramters for creating data, passing the new req.body and req.parms
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData[0]) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+    User.destroy({ //destroy deleyes sepcified data
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
